8692fa066aea3322ac0664381b91d40ba1c0167a
Rahul Bhoje <rahulbhoje09@gmail.com>
2025-06-10 13:19:17 +0530

    Basic Circuit breaker fallback in api gateway implementation

--- src/main/java/com/javatechie/controller/FallbackController.java
+++ src/main/java/com/javatechie/controller/FallbackController.java
@@ -2,6 +2,7 @@ package com.javatechie.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/fallback")
 public class FallbackController {
 
+    @GetMapping("/order")
     public ResponseEntity<String> orderFallback(){
         return ResponseEntity.status(HttpStatus.GATEWAY_TIMEOUT)
-                .body("we are facing a problem in order-service!, Please contact help desk.");
+                .body("We are facing a problem in order-service!, Please contact help desk.");
     }
+
+    @GetMapping("/payment")
+    public ResponseEntity<String> paymentFallback(){
+        return ResponseEntity.status(HttpStatus.GATEWAY_TIMEOUT)
+                .body("We are facing a problem in payment-service!, Please contact help desk.");
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<String> userFallback(){
+        return ResponseEntity.status(HttpStatus.GATEWAY_TIMEOUT)
+                .body("We are facing a problem in user-service!, Please contact help desk.");
+    }
+
+
 }
